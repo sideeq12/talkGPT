01ee9e29d68ee3aa8d933854c50b587e7af133f1
sideeq12 <abdwaheed2018@gmail.com>
2023-03-08 15:11:26 +0000

    checking auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_KEY,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     // ...add more providers here
   ],

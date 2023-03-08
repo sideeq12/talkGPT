68b04dd4e032ddf4302208024b3896b555b26b8b
sideeq12 <abdwaheed2018@gmail.com>
2023-03-08 23:24:58 +0000

    toaster

---
+++ components/ClientProvider.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Toaster } from "react-hot-toast";
+
+export default function ClientProvider(){
+  return (
+    <div>
+        <Toaster  position="top-right"/>
+    </div>
+  )
+}

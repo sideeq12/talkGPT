a94a96fee0ad98d9f03630ffe0c93884d4738aa2
sideeq12 <abdwaheed2018@gmail.com>
2023-03-08 11:55:28 +0000

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex flex-col justify-center items-center h-screen px-2 text-white'>
+        <h1 className='text-5xl font-bold mb-20'>TalkGPT</h1>
+
+        <div>
+            <div>
+                <div className='flex flex-col items-center mb-5 justify-center'>
+                    {/* sun icons */}
+                    <p>Examples</p>
+                </div>
+                <div className='space-y-2'>
+                    <p className='infoText'> still thintkingn</p>
+                    <p className='infoText'> still thintkingn</p>
+                    <p className='infoText'> still thintkingn</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 

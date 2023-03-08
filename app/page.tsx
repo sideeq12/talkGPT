710f9120161bcf0fcb7e6d4ffd0d57a46cf3ab7b
sideeq12 <abdwaheed2018@gmail.com>
2023-03-08 12:01:23 +0000

    custom layer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SunIcon } from '@heroicons/react/24/solid'
 
 const Home = () => {
   return (
@@ -8,13 +9,13 @@ const Home = () => {
         <div>
             <div>
                 <div className='flex flex-col items-center mb-5 justify-center'>
-                    {/* sun icons */}
+                <SunIcon className="h-8 w-8"/>
                     <p>Examples</p>
                 </div>
                 <div className='space-y-2'>
-                    <p className='infoText'> still thintkingn</p>
-                    <p className='infoText'> still thintkingn</p>
-                    <p className='infoText'> still thintkingn</p>
+                    <p className='infoText'>Explain something to me</p>
+                    <p className='infoText'> What is the difference between a cat and Dog</p>
+                    <p className='infoText'> Who is Peter Obi</p>
                 </div>
             </div>
         </div>

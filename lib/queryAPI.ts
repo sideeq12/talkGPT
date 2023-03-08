d2ee51de53c9fac0c864f3ae97e7f3a495487b7a
sideeq12 <abdwaheed2018@gmail.com>
2023-03-08 23:59:45 +0000

    layer

---
+++ lib/queryAPI.ts
@@ -0,0 +1,17 @@
+import openai from "./chatGPT";
+
+
+const query =async (prompt : string, chatId: string, model: string) => {
+    const res = await openai.createCompletion({
+        model,
+        prompt,
+        temperature : 0.9,
+        top_p : 1,
+        frequency_penalty : 0,
+        presence_penalty : 0,
+        max_tokens :1000
+    }).then(res => res.data.choices[0].text).catch(err =>`ChatGPT is unabke to find the answer for that : ${err.message}`)
+    return res;
+}
+
+export default  query

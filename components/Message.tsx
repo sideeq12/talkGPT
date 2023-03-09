59ba357c8a3eb679fe9484ed160bed0bb3d8ff63
sideeq12 <abdwaheed2018@gmail.com>
2023-03-09 02:03:22 +0000

    functioning link

--- components/Message.tsx
+++ components/Message.tsx
@@ -7,7 +7,7 @@ type Props ={
 const Message = ({ message } : Props) => {
     const isChatGPT = message.user.name === "ChatGPT"
   return (
-    <div className={`py-5 text-white ${isChatGPT && "bg-[#4343654]"}`}>
+    <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
         <div className='flex space-x-5 px-10 max-w-2xl mx-auto'>
             <img src={message.user.avatar} className="h-8 w-8" alt="" />
             <p className='pt-1 text-sm'>{message.text}</p>

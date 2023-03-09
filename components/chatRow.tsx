7c45e501487bbd7125690797175262e3df398a39
sideeq12 <abdwaheed2018@gmail.com>
2023-03-09 07:16:17 +0000

    fix : chatRow

--- components/chatRow.tsx
+++ components/chatRow.tsx
@@ -16,10 +16,9 @@ const ChatRow = ({id} : Props) => {
     const router = useRouter();
     const { data : session } = useSession();
     const [active, setActive ] = useState(false)
-    const [messages ] = useCollection(query(
-        collection(db, "user", session?.user?.email!, "chats", id, "messages"),
-        orderBy("createdAt", "asc")
-    ))
+
+    const [messages]  = useCollection(session && query(collection(db, "users", session.user?.email!, "chats",
+    id, "messages"), orderBy("createdAt", "asc") ))
     console.log("we are check", messages?.docs)
     useEffect(()=>{
         if(!pathName) return;
@@ -34,7 +33,7 @@ const ChatRow = ({id} : Props) => {
     <Link href={`/chats/${id}`} className={`chatRow justify-center ${ active && "bg-gray-700/50"}`}>
         <ChatBubbleLeftIcon className='h-5 w-5'/>
         <p className='flex-1 hidden md:inline-flex truncate'>
-            {messages?.docs[messages?.docs.length-1]?.data().text || "New Chat"}
+            {messages?.docs[0]?.data().text || "New Chat"}
         </p>
         <TrashIcon onClick={removeChat}  className='h-5 w-5 text-gray-700 hover:text-red-700' />
     </Link>
